60025d7fdfedf2075ae3216abe6178a59654ab9f
Jack Estes <jackestes3210@gmail.com>
2025-03-24 19:47:22 -0600

    Finished

--- backend/Bookstore/Bookstore/Controllers/BookController.cs
+++ backend/Bookstore/Bookstore/Controllers/BookController.cs
@@ -15,13 +15,28 @@ namespace Bookstore.Controllers
         }
 
         [HttpGet(Name = "GetBooks")]
-        public IActionResult GetBooks()
+        public IActionResult GetBooks(int cardsPerPage = 5, int pageNum = 1, bool sortByName = false)
         {
             var query = _context.Books.AsQueryable();
             
-            var bookList = query.ToList();
+            if (sortByName)
+            {
+                query = query.OrderBy(b => b.Title);
+            }
             
-            return Ok(bookList);
+            var numBooks = query.Count();
+            var bookList = query
+                .Skip((pageNum - 1) * cardsPerPage)
+                .Take(cardsPerPage)
+                .ToList();
+
+            var booksData = new
+            {
+                Books = bookList,
+                NumBooks = numBooks,
+            };
+            
+            return Ok(booksData);
         }
     }
 }
